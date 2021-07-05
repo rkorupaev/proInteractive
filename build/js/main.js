abe2fcf7bc8f350af7df3f67047e898772f613f0
Roman Korupaev <rkorupaev@gmail.com>
2021-07-05 22:23:08 +0300

    additional features in progrees

--- build/js/main.js
+++ build/js/main.js
@@ -90,35 +90,10 @@
 /*!*****************************!*\
   !*** ./js/modules/index.js ***!
   \*****************************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/*! no static exports found */
+/***/ (function(module, exports) {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _map_cards__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./map_cards */ \"./js/modules/map_cards.js\");\n/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./utils */ \"./js/modules/utils.js\");\n\n\nvar DOWNLOAD_URL = \"https://603e38c548171b0017b2ecf7.mockapi.io/homes\";\nvar STATUS_CODE_OK = \"200\";\nvar FILTER_INPUT_CLASS = \".filter-block__input\";\nvar ERROR_MESSAGE = \".filter-block__error-message\";\nvar APART_LIST = \".main-body__aparts-list\";\nvar MIN_INPUT_VALUE_LENGTH = 3;\nvar DELAY = 250;\n\nvar fetchData = function fetchData(onLoad, onError) {\n  var xhr = new XMLHttpRequest();\n  xhr.responseType = 'json';\n  xhr.addEventListener('load', function () {\n    if (xhr.readyState === 4 && xhr.status == STATUS_CODE_OK) {\n      onLoad(xhr.response);\n    } else {\n      onError(xhr.status);\n    }\n  });\n  xhr.addEventListener('error', function () {\n    onError('Произошла ошибка. Обратитесь к администратору.');\n  });\n  xhr.addEventListener('timeout', function () {\n    onError('Превышено время ожидания ответа от сервера.');\n  });\n  xhr.timeout = 10000;\n  xhr.open('GET', DOWNLOAD_URL, true);\n  xhr.send();\n};\n\nfetchData(function (data) {\n  if (data) {\n    var filteredData = data;\n    var cardBlock = document.querySelector(APART_LIST);\n    Object(_map_cards__WEBPACK_IMPORTED_MODULE_0__[\"mapCards\"])(filteredData, cardBlock);\n    var filterInput = document.querySelector(FILTER_INPUT_CLASS);\n    var errorMessage = document.querySelector(ERROR_MESSAGE);\n    console.log(errorMessage);\n\n    var onChange = function onChange(evt) {\n      if (evt.target.value.length > MIN_INPUT_VALUE_LENGTH) {\n        errorMessage.style.display = \"none\";\n        filteredData = filterData(data, evt);\n        Object(_map_cards__WEBPACK_IMPORTED_MODULE_0__[\"mapCards\"])(filteredData, cardBlock);\n      } else if (evt.target.value.length === 0) {\n        errorMessage.style.display = \"none\";\n        Object(_map_cards__WEBPACK_IMPORTED_MODULE_0__[\"mapCards\"])(data, cardBlock);\n      } else {\n        errorMessage.style.display = \"block\";\n      }\n    };\n\n    onChange = Object(_utils__WEBPACK_IMPORTED_MODULE_1__[\"debounce\"])(onChange, DELAY);\n    filterInput.addEventListener(\"input\", onChange);\n  }\n}, function (status) {\n  console.log(status);\n});\n\nvar filterData = function filterData(data, evt) {\n  var filteredData = data.filter(function (appart) {\n    return appart.title.toLowerCase().includes(evt.target.value.toLowerCase());\n  });\n  return filteredData;\n};\n\n//# sourceURL=webpack:///./js/modules/index.js?");
-
-/***/ }),
-
-/***/ "./js/modules/map_cards.js":
-/*!*********************************!*\
-  !*** ./js/modules/map_cards.js ***!
-  \*********************************/
-/*! exports provided: mapCards */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"mapCards\", function() { return mapCards; });\n/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ \"./js/modules/utils.js\");\n\nvar INDEPENDENT_LIVING = \"Independent living\";\nvar SUPPORT_AVALIABLE = \"Restaurant & Support available\";\nvar MAIN_BODY_BUTTON = \".main-body__button\";\nvar APARTS_LIST_ITEM = \"aparts-list__item\";\nvar CARD_IMAGE = \".card__image\";\nvar CARD_TYPE = \".card__type\";\nvar CARD_TITLE = \".card__title\";\nvar CARD_ADDRESS = \".card__address\";\nvar CARD_PRICE = \".card__price\";\nvar CARD_ORANGE_BACKGROUND = \"card__type--orange-background\";\nvar mapCards = function mapCards(data, block) {\n  block.textContent = \"\";\n  var seeMoreButton = document.querySelector(MAIN_BODY_BUTTON);\n\n  if (data.length == 0) {\n    seeMoreButton.style.display = \"none\";\n    var notification = document.createElement(\"div\");\n    notification.classList.add(\"notification\");\n    notification.append(document.getElementById(\"notification\").content.cloneNode(true));\n    block.append(notification);\n  } else {\n    seeMoreButton.style.display = \"block\";\n\n    for (var i = 0; i < 6; i++) {\n      if (data[i]) {\n        var card = document.createElement(\"li\");\n        card.classList.add(APARTS_LIST_ITEM);\n        card.append(document.getElementById(\"card\").content.cloneNode(true));\n        var imageIndex = Object(_utils__WEBPACK_IMPORTED_MODULE_0__[\"getRandomInt\"])(1, 5);\n        card.querySelector(CARD_IMAGE).setAttribute(\"src\", \"./img/apart\" + imageIndex + \".png\");\n\n        switch (data[i].type) {\n          case \"IndependentLiving\":\n            card.querySelector(CARD_TYPE).textContent = INDEPENDENT_LIVING;\n            break;\n\n          case \"SupportAvailable\":\n            card.querySelector(CARD_TYPE).classList.add(CARD_ORANGE_BACKGROUND);\n            card.querySelector(CARD_TYPE).textContent = SUPPORT_AVALIABLE;\n            break;\n        }\n\n        card.querySelector(CARD_TITLE).textContent = data[i].title;\n        card.querySelector(CARD_ADDRESS).textContent = data[i].address;\n        card.querySelector(CARD_PRICE).textContent = \"£\" + data[i].price;\n        block.append(card);\n      }\n    }\n  }\n};\n\n//# sourceURL=webpack:///./js/modules/map_cards.js?");
-
-/***/ }),
-
-/***/ "./js/modules/utils.js":
-/*!*****************************!*\
-  !*** ./js/modules/utils.js ***!
-  \*****************************/
-/*! exports provided: getRandomInt, debounce */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"getRandomInt\", function() { return getRandomInt; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"debounce\", function() { return debounce; });\nvar getRandomInt = function getRandomInt(min, max) {\n  return Math.floor(Math.random() * (max - min)) + min;\n};\nvar debounce = function debounce(callback, delay) {\n  var timeout;\n  return function () {\n    var _arguments = arguments,\n        _this = this;\n\n    var fnCall = function fnCall() {\n      callback.apply(_this, _arguments);\n    };\n\n    clearTimeout(timeout);\n    timeout = setTimeout(fnCall, delay);\n  };\n};\n\n//# sourceURL=webpack:///./js/modules/utils.js?");
+eval("var photoBoothSwiper = function photoBoothSwiper(el) {\n  return new Swiper(el, {\n    // Optional parameters\n    direction: \"horizontal\",\n    loop: true,\n    grabCursor: true,\n    speed: 1500,\n    // Navigation arrows\n    navigation: {\n      nextEl: \".swiper-button-next\",\n      prevEl: \".swiper-button-prev\"\n    }\n  });\n};\n\nvar photoBoothItem = document.querySelector('.photo-booth__slider');\n\nif (photoBoothItem) {\n  photoBoothSwiper(photoBoothItem);\n}\n\n//# sourceURL=webpack:///./js/modules/index.js?");
 
 /***/ })
 
--- source/js/vendor/index.js
+++ source/js/vendor/index.js
@@ -1,2 +1,3 @@
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+import * as Swiper from '../vendor/swiper.js';
