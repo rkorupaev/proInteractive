456cf78c9ada2250d885dc1a39523e6953f69266
Roman <rkorupaev@gmail.com>
2021-07-30 14:17:43 +0300

    video block rework

--- source/js/vendor/index.js
+++
@@ -1,4 +0,0 @@
-import 'lazysizes';
-import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-import SwiperCore, { Navigation, Pagination } from 'swiper/core';
-SwiperCore.use([Navigation, Pagination]);
