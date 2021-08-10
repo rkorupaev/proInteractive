f65c12e2e5236dfed8051a88ee7c93346efe89ee
Korupaev Roman <r.korupaev@norsi-trans.lcl>
2021-08-10 12:21:24 +0300

    news cards mapped, button logic added

--- source/js/modules/index.js
+++ source/js/modules/index.js
@@ -90,7 +90,7 @@ for (let i = 0; i < 3; i++) {
     extraOption.classList.add(`extra-options__item`);
     extraOption.append(document.getElementById(`extra-option`).content.cloneNode(true));
     extraOption.querySelector(`.extra-options__item-title`).textContent = `Разработка макета рамки #` + (j + 1);
-    extraOption.querySelector(`.extra-options__item-price`).textContent = `от `+ (17500 + 100 * j)  + `₽`;
+    extraOption.querySelector(`.extra-options__item-price`).textContent = `от ` + (17500 + 100 * j) + `₽`;
 
     extraOptionsBlock.append(extraOption);
   }
@@ -99,7 +99,8 @@ for (let i = 0; i < 3; i++) {
 }
 
 // swiper initiation
-import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+import SwiperCore, {Navigation, Pagination} from 'swiper/core';
+
 SwiperCore.use([Navigation, Pagination]);
 
 const swiper = new SwiperCore('.swiper-container', {
@@ -134,5 +135,39 @@ faqItemsList.forEach((item, index) => {
     faqArrow.classList.toggle(`faq__item-arrow--active`);
     faqTitleWrapper.classList.toggle(`faq__title-wrapper--active`);
   });
+});
+
+//map news cards
+const MAX_NEWS_LENGTH = 9;
+const NEWS_PER_CLICK = 3;
+
+const mapNews = (cardCount) => {
+  for (let i = 0; i < cardCount; i++) {
+    const newsCard = document.createElement(`li`);
+    const newsBlock = document.querySelector(`.news__list`);
+    newsCard.classList.add(`news__item`);
+    newsCard.append(document.getElementById(`news-card`).content.cloneNode(true));
+
+    newsBlock.append(newsCard);
+  }
+}
+
+window.addEventListener(`load`, (evt) => {
+  mapNews(NEWS_PER_CLICK);
+});
+
+const showMoreNewsButton = document.querySelector(`.news__button`);
+showMoreNewsButton.addEventListener('click', (evt) => {
+  const newsLength = document.querySelectorAll(`.news__item`).length;
+  const lengthDelta = MAX_NEWS_LENGTH - newsLength;
+  if (newsLength < MAX_NEWS_LENGTH && lengthDelta >= 3) {
+    mapNews(NEWS_PER_CLICK);
+  } else {
+    mapNews(lengthDelta);
+  }
+
+  if (MAX_NEWS_LENGTH <= newsLength + NEWS_PER_CLICK) {
+    showMoreNewsButton.style.display = `none`;
+  }
 });
 
